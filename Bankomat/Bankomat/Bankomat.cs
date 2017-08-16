f2652b5ab7b6a2187d923dffd1ba37e38fc89798
Yakov-Podskarbiy <pjn92@mail.ru>
2017-08-16 17:49:40 +0700

    Создание классов и методов, ввод суммы в банкомат, проверка суммы на корректность.

---
+++ Bankomat/Bankomat/Bankomat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+   public class Bankomat
+    {
+        public string b_id;
+        public  string _50;
+        public string _100;
+        public string _500;
+        public string _1000;
+        public string _5000;
+
+        public int read_sum;
+
+        public int sum;
+       
+       public void Input ( )
+       
+       {
+           Console.WriteLine("ID:");
+
+           b_id = Console.ReadLine();
+          
+           Console.WriteLine("50:");
+
+           _50 = Console.ReadLine();
+
+           Console.WriteLine("100:");
+
+           _100 = Console.ReadLine();
+
+           Console.WriteLine("500:");
+
+           _500 = Console.ReadLine();
+
+           Console.WriteLine("1000:");
+
+           _1000 = Console.ReadLine();
+
+           Console.WriteLine("5000:");
+
+           _5000 = Console.ReadLine();
+
+           sum = Int32.Parse(_50)*50 + Int32.Parse(_100)*100 + Int32.Parse(_500)*500 + Int32.Parse(_1000)*1000 + Int32.Parse(_5000)*5000;
+           
+       }
+
+       public void Output()
+
+       {
+
+ 
+       }
+
+       public void Check(string input_sum)
+       {
+           if(input_sum < sum)
+
+       }
+     
+
+      
+       
+       
+           
+       
+
+    }
+
+   
+
+
+
+}
--- Bankomat/Bankomat/Program.cs
+++ Bankomat/Bankomat/Program.cs
@@ -10,6 +10,30 @@ namespace Bankomat
     {
         static void Main(string[] args)
         {
+            Bankomat b1 = new Bankomat();
+
+            b1.Input();
+
+            Console.WriteLine("Введите сумму:");
+
+            b1.read_sum = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выдать с разменом?  1 - Да;   2 - Нет");
+
+            string quest = Console.ReadLine();
+
+            b1.Check(quest);
+
+            if (Int32.Parse(quest) == 1)
+            {
+                b1.Output_1();
+            }
+
+
+
+                       
+            
+            
         }
     }
 }
